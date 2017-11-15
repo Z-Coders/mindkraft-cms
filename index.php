360c1b6db0796b9cf1dfc7a6060d50cb2f86c19a
Mihir Pathak <mihirpathak97@gmail.com>
2017-11-15 10:42:39 +0530

    stylize login

--- index.php
+++ index.php
@@ -8,15 +8,30 @@
   <head>
     <meta charset="utf-8">
     <title>MindKraft CMS</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/cms-master.css">
   </head>
   <body>
-    <form class="" action="input.php" method="post">
-      <select class="" name="opt">
-        <option value="games">Games</option>
-        <option value="events">Events</option>
-        <option value="workshops">Workshops</option>
-      </select>
-      <input type="submit" name="" value="Go!">
-    </form>
+    <div class="container">
+        <div class="jumbotron">
+          <div class="container">
+            <h1 class="display-3">CMS - Login</h1>
+            <br>
+            <form class="form-inline" action="cms/authenticate.php" id="login-form" method="post">
+              <div class="form-group">
+                <label for="email" id="user">Username :</label>
+                <input type="text" class="form-control" id="email" placeholder="Your ID here." required>
+                </div>
+                <p></p>
+                <div class="form-group">
+                <label for="password" id="pass">Password :</label>
+                <input type="password" class="form-control" id="password" placeholder="Your password here." required>
+              </div>
+            </form>
+            <hr class="m-y-md">
+            <input type="submit" form="login-form" name="" value="Login" class="btn btn-primary btn-lg">
+        </div>
+      </div>
+      </div>
   </body>
 </html>
