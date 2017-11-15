757e86f7cb18f974748fe7dc2ab6a234238e32da
Mihir Pathak <mihirpathak97@gmail.com>
2017-11-15 15:53:50 +0530

    min stylize addevent

--- cms/addevent.php
+++ cms/addevent.php
@@ -14,10 +14,33 @@
   $query2 = "insert into ".$table_prefix."all_events values ('".$name."', '".$id."')";
   $result = $sql->execQuery($query);
   $result2 = $sql->execQuery($query2);
-  if ($result && $result2) {
-    echo "Inserted Successfully";
-  }
-  else {
-    echo "Error inserting data!";
-  }
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Add Event</title>
+  </head>
+  <style media="screen">
+    .container{
+      display: block;
+      margin: auto;
+      text-align: center;
+      margin-top: 15%;
+    }
+  </style>
+  <body>
+    <div class="container">
+      <?php
+        if ($result && $result2) {
+          echo "<h1>Added event Successfully...</h1>";
+        }
+        else {
+          echo "<h1>Error inserting data!<h1>";
+        }
+      ?>
+      <a href="console.php">Home</a>
+    </div>
+  </body>
+</html>
