0fa28c3377e3380166aa480ce0973b1897776af9
Mihir Pathak <mihirpathak97@gmail.com>
2017-11-16 16:27:08 +0530

    mod query for password hashing

--- cms/authenticate.php
+++ cms/authenticate.php
@@ -5,7 +5,7 @@
   $user = $_POST['user'];
   $password = $_POST['password'];
   $table_prefix = $sql->getTeblePrefix();
-  $query = "select * from ".$table_prefix."cpanel_users where username='".$user."' and password='".$password."'";
+  $query = "select * from ".$table_prefix."cpanel_users where username='".$user."' and password=password('".$password."')";
   $result = $sql->execQuery($query);
   if (mysqli_num_rows($result) == 1) {
     $_SESSION['user'] = true;
