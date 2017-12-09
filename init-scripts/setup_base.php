4e8b86929047bef0a30bb8e2a3c762e035cee36d
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-09 13:51:55 +0530

    setup_base: add comments

--- init-scripts/setup_base.php
+++ init-scripts/setup_base.php
@@ -22,7 +22,64 @@
   $root_sql = parse_ini_file('sqlconf.ini', true)['sqlconf-root'];
   $connection_var = mysqli_connect($root_sql['sql_hostname'], $root_sql['sql_username'], $root_sql['sql_password']);
 
+  // Master tables list. Add table here and then define in t_struct (please maintain the same order)
   $t_list = array('cpanel_users', 'enduser_table', 'events_list', 'games_list', 'workshops_list', 'all_events');
+
+  //  Table Structure
+  //
+  //  cpanel_users :
+  //    cpanel_username varchar(64)
+  //    user_password varbinary(512)
+  //
+  //  enduser_table :
+  //    userid varchar(32) not null unique
+  //    enduser_mobile varchar(16) not null unique
+  //    enduser_name varchar(64) not null
+  //    enduser_email varchar(64) not null unique
+  //    enduser_college_name varchar(128)
+  //    enduser_password varbinary(512) not null
+  //
+  //  events_list :
+  //    event_name varchar(64)
+  //    event_id varchar(32) not null unique
+  //    event_type varchar(32)
+  //    event_department varchar(64)
+  //    event_incharge varchar(128)
+  //    incharge_contact varchar(128)
+  //    event_fee varchar(16)
+  //    event_prize varchar(16)
+  //    event_description mediumtext
+  //
+  //  games_list :
+  //    event_name varchar(64)
+  //    event_id varchar(32) not null unique
+  //    event_incharge varchar(128)
+  //    incharge_contact varchar(128)
+  //    event_fee varchar(16)
+  //    event_prize varchar(16)
+  //    event_description mediumtext
+  //
+  //  workshops_list :
+  //    event_name varchar(64)
+  //    event_id varchar(32) not null unique
+  //    event_department varchar(64)
+  //    event_incharge varchar(128)
+  //    incharge_contact varchar(128)
+  //    event_fee varchar(16)
+  //    event_description mediumtext
+  //
+  //  all_events :
+  //    event_name varchar(64)
+  //    event_id varchar(32) not null unique
+  //    event_type varchar(32)
+  //    category varchar(32)
+  //    event_department varchar(64)
+  //    event_incharge varchar(128)
+  //    incharge_contact varchar(128)
+  //    event_fee varchar(16)
+  //    event_prize varchar(16)
+  //    event_description mediumtext
+
   $t_struct = array(
     'cpanel_users' => 'cpanel_username varchar(64), user_password varbinary(512)',
     'enduser_table' => 'userid varchar(32) not null unique, enduser_mobile varchar(16) not null unique, enduser_name varchar(64) not null, enduser_email varchar(64) not null unique, enduser_college_name varchar(128), enduser_password varbinary(512) not null',
@@ -32,6 +89,7 @@
     'all_events' => 'event_name varchar(64), event_id varchar(32) not null unique, event_type varchar(32), category varchar(32), event_department varchar(64), event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_prize varchar(16), event_description mediumtext',
   );
 
+  // Create database first
   $query = "create database " . $root_sql['sql_database'];
   $result = mysqli_query($connection_var, $query);
 
