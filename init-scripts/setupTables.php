5b0c7184edc7bf93666d909aaee1f25e3e34b4de
Mihir Pathak <mihirpathak97@gmail.com>
2017-12-09 11:37:33 +0530

    setupTables: use assoc array for t_struct

--- init-scripts/setupTables.php
+++ init-scripts/setupTables.php
@@ -1,5 +1,4 @@
 <?php
-
   $sqlconf_file = fopen("sqlconf.ini", "w");
   $prefix_text =
 "; SQL Configuration file. Automatically generated when init.php is run\n
@@ -8,7 +7,7 @@
   fwrite($sqlconf_file, $prefix_text);
 
   fwrite($sqlconf_file, "\n");
-  fwrite($sqlconf_file, "[sqlconf-user]\n");
+  fwrite($sqlconf_file, "[sqlconf-root]\n");
   fwrite($sqlconf_file, "sql_hostname = \"".$_POST['MySQLServerHost']."\"\n");
   fwrite($sqlconf_file, "sql_username = \"".$_POST['MySQLUsername']."\"\n");
   fwrite($sqlconf_file, "sql_password = \"".$_POST['MySQLPassword']."\"\n");
@@ -20,19 +19,20 @@
 
   copy("sqlconf.ini", "../cms/sqlconf.ini");
 
-  $user_sql = parse_ini_file('sqlconf.ini', true)['sqlconf-user'];
-  $connection_var = mysqli_connect($user_sql['sql_hostname'], $user_sql['sql_username'], $user_sql['sql_password']);
-  $table_list = array('cpanel_users', 'enduser_table', 'events_list', 'games_list', 'workshops_list', 'all_events');
-  $table_config = array(
-                  "username varchar(64), password varbinary(512)",
-                  "userid varchar(32) not null unique, mobile varchar(16) not null unique, name varchar(64) not null, email varchar(64) not null unique, college varchar(128), password varbinary(512) not null",
-                  "event_name varchar(64), event_id varchar(32) not null unique, event_type varchar(32), department varchar(64), event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_prize varchar(16), description mediumtext",
-                  "event_name varchar(64), event_id varchar(32) not null unique, event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_prize varchar(16), description mediumtext",
-                  "event_name varchar(64), event_id varchar(32) not null unique, department varchar(64), event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), description mediumtext",
-                  "event_name varchar(64), event_id varchar(32) not null unique, event_type varchar(32), category varchar(32), department varchar(64), event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_prize varchar(16), description mediumtext"
-                );
+  $root_sql = parse_ini_file('sqlconf.ini', true)['sqlconf-root'];
+  $connection_var = mysqli_connect($root_sql['sql_hostname'], $root_sql['sql_username'], $root_sql['sql_password']);
 
-  $query = "create database " . $user_sql['sql_database'];
+  $t_list = array('cpanel_users', 'enduser_table', 'events_list', 'games_list', 'workshops_list', 'all_events');
+  $t_struct = array(
+    'cpanel_users' => 'cpanel_username varchar(64), user_password varbinary(512)',
+    'enduser_table' => 'userid varchar(32) not null unique, enduser_mobile varchar(16) not null unique, enduser_name varchar(64) not null, enduser_email varchar(64) not null unique, enduser_college_name varchar(128), enduser_password varbinary(512) not null',
+    'envents_list' => 'event_name varchar(64), event_id varchar(32) not null unique, event_type varchar(32), event_department varchar(64), event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_prize varchar(16), event_description mediumtext',
+    'games_list' => 'event_name varchar(64), event_id varchar(32) not null unique, event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_prize varchar(16), event_description mediumtext',
+    'workshops_list' => 'event_name varchar(64), event_id varchar(32) not null unique, event_department varchar(64), event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_description mediumtext',
+    'all_events' => 'event_name varchar(64), event_id varchar(32) not null unique, event_type varchar(32), category varchar(32), event_department varchar(64), event_incharge varchar(128), incharge_contact varchar(128), event_fee varchar(16), event_prize varchar(16), event_description mediumtext',
+  );
+
+  $query = "create database " . $root_sql['sql_database'];
   $result = mysqli_query($connection_var, $query);
 
   if ($result) {
@@ -43,31 +43,33 @@
     return;
   }
 
-  $connection_var = mysqli_connect($user_sql['sql_hostname'], $user_sql['sql_username'], $user_sql['sql_password'], $user_sql['sql_database']);
+  $connection_var = mysqli_connect($root_sql['sql_hostname'], $root_sql['sql_username'], $root_sql['sql_password'], $root_sql['sql_database']);
 
   echo "Creating tables...<br>";
-  for ($i=0; $i < count($table_list); $i++) {
-    $query = "create table " . $user_sql['sql_table_prefix'].$table_list[$i] . "(" . $table_config[$i] . ")";
+  foreach ($t_struct as $t_name => $t_config) {
+    $query = "create table " . $root_sql['sql_table_prefix'].$t_name . "(" . $t_config . ")";
     $result = mysqli_query($connection_var, $query);
     if ($result) {
-      echo "Created table " . $user_sql['sql_table_prefix'].$table_list[$i] . "<br>";
+      echo "Created table " . $root_sql['sql_table_prefix'].$t_name . "<br>";
     }
     else {
-      echo "Error creating table " . $user_sql['sql_table_prefix'].$table_list[$i] . "<br>" . "Quitting!";
-      break;
+      echo "Error creating table " . $root_sql['sql_table_prefix'].$t_name . "<br>" . "Quitting!";
+      return;
     }
   }
 
-  foreach ($table_list as $table) {
-    $query = "create view ".$user_sql['sql_view_prefix'].$table." as select * from ".$user_sql['sql_table_prefix'].$table;
+  foreach ($t_list as $table) {
+    $query = "create view ".$root_sql['sql_view_prefix'].$table." as select * from ".$root_sql['sql_table_prefix'].$table;
     $result = mysqli_query($connection_var, $query);
     if ($result) {
-      echo "Created view for ".$user_sql['sql_table_prefix'].$table." as ".$user_sql['sql_view_prefix'].$table."<br>";
+      echo "Created view for ".$root_sql['sql_table_prefix'].$table." as ".$root_sql['sql_view_prefix'].$table."<br>";
     }
     else {
       echo "Error creating view...<br>Quitting!";
-      break;
+      return;
     }
   }
+
+  echo "<a href=\"create_users.php\">Go Ahead</a>";
 
 ?>
